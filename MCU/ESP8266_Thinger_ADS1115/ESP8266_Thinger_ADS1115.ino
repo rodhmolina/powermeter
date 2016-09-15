917438549c40c4091c64552b9a071b5c90c41a4f
rodrigo molina <rodrigo molina>
2016-09-14 22:18:11 -0300

    inicio

---
+++ MCU/ESP8266_Thinger_ADS1115/ESP8266_Thinger_ADS1115.ino
@@ -0,0 +1,66 @@
+#include <Wire.h>
+
+#include <Adafruit_ADS1015.h>
+
+#include <ESP8266WiFi.h>
+#include <ThingerWifi.h>
+
+#define USERNAME "your_user_name"
+#define DEVICE_ID "your_device_id"
+#define DEVICE_CREDENTIAL "your_device_credential"
+
+#define SSID "your_wifi_ssid"
+#define SSID_PASSWORD "your_wifi_ssid_password"
+
+ThingerWifi thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+
+int16_t CURRENT; /* corriente del SCP013*/
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);
+
+  thing.add_wifi(SSID, SSID_PASSWORD);
+
+  // digital pin control example (i.e. turning on/off a light, a relay, configuring a parameter, etc)
+  thing["led"] << digitalPin(BUILTIN_LED);
+
+  // resource output example (i.e. reading a sensor value)
+  thing["current"] >> outputValue(millis());
+
+  // more details at http://docs.thinger.io/arduino/
+
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+   ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  
+  ads.begin();
+}
+
+void loop() {
+  
+
+  int16_t results;
+  
+  /* Be sure to update this value based on the IC and the gain settings! */
+  //float   multiplier = 3.0F;    /* ADS1015 @ +/- 6.144V gain (12-bit results) */
+  float multiplier = 0.125F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
+
+  results = ads.readADC_Differential_0_1();  
+  CURRENT = results * multiplier;
+  
+  //Serial.print("Differential: "); Serial.print(results); Serial.print("("); Serial.print(results * multiplier); Serial.println("mV)");
+
+  thing.handle();
+  delay(1000);
+}
